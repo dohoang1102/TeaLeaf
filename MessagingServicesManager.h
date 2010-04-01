bd0afc3f5600cbad122ac9fb05257416f43b2c4a
Richard Turnbull <richard.turnbull@gmail.com>
2010-04-01 19:52:20 +0100

    added functions to Services Manager class

--- MessagingServicesManager.h
+++ MessagingServicesManager.h
@@ -2,6 +2,11 @@
 //  MessagingServicesManager.h
 //  TeaLeaf
 //
+//  Keeps array of messaging service instances
+//		-dispatches messages to them
+//		-receives messages from them
+//
+//
 //  Created by Richard Turnbull on 31/03/2010.
 //  Copyright 2010 Swandrift Consulting Limited. All rights reserved.
 //
@@ -12,17 +17,24 @@
 
 @interface MessagingServicesManager : NSObject {
 	
-	NSMutableArray	*messagingServices;
+	NSMutableArray	*serviceInstances;
 	id				delegate;
 
 }
 
 @property (assign, nonatomic, readwrite) id delegate;
+@property (retain, nonatomic, readwrite) NSMutableArray *serviceInstances;
 
 
--(id)initWithDelegate:(id)delegate;
--(void)startListeningOnServiceInstance:(MessagingService *)messagingService
-
+-(id)initWithDelegate:(id)delegate config:(NSDictionary *)configDictionary;
+-(void)startServiceInstance:(MessagingService *)serviceInstance;
+-(void)stopServiceInstance:(MessagingService *)serviceInstance;
+-(void)startAllServiceInstances;
+-(void)stopAllServiceInstances;
+-(void)sendAttachment:(NSData *)attachment;
+-(void)sendAttachment:(NSData *)attachment usingServiceInstance:(MessagingService *)serviceInstance;
+-(void)sendMessageText:(NSString *)messageText;
+-(void)sendMessageText:(NSString *)messageText usingServiceInstance:(MessagingService *)serviceInstance;
 
 
 @end
